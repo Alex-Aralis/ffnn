c6e31de02a75d7703114a99c40820b27e9f7b713
Alex Aralis <alex.aralis@gmail.com>
2017-05-31 19:49:15 -0400

    add a test for EvalNN

--- util/eval_nn.go
+++ util/eval_nn.go
@@ -2,13 +2,16 @@ package util
 
 import "gonum.org/v1/gonum/matrix/mat64"
 
-// EvalNN takes a vector y of input values and
+// EvalNN takes a vector x of input values and
 // runs it through the neral net represented by
 // layers.
-func EvalNN(y *mat64.Dense, layers []*mat64.Dense) *mat64.Dense {
+func EvalNN(x *mat64.Dense, layers []*mat64.Dense) *mat64.Dense {
 	for _, m := range layers {
-		y.Mul(m.T(), y)
+		var y mat64.Dense
+
+		y.Mul(m.T(), x)
+		x = LogisticActivation(&y)
 	}
 
-	return y
+	return x
 }
---
+++ util/eval_nn_test.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+
+	"gonum.org/v1/gonum/matrix/mat64"
+)
+
+func TestEvalNN(t *testing.T) {
+	layers := []*mat64.Dense{
+		makeOnesMatrix(4, 3),
+		makeOnesMatrix(3, 3),
+		makeOnesMatrix(3, 2),
+	}
+
+	x := mat64.NewDense(4, 1, []float64{2, 1, 2, 3})
+
+	y := EvalNN(x, layers)
+
+	fmt.Println(y)
+}
+
+func makeOnesMatrix(r, c int) *mat64.Dense {
+	m := mat64.NewDense(r, c, nil)
+
+	m.Apply(
+		func(i, j int, val float64) float64 {
+			return 1
+		},
+		m,
+	)
+
+	return m
+}
