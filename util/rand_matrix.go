7f11b3968b7102375700c0fdf01e4de894fe491b
Alex Aralis <alex.aralis@gmail.com>
2017-05-31 15:11:06 -0400

    change to using a source based on the first call of the rand gen function instead of the default seed

--- util/rand_matrix.go
+++ util/rand_matrix.go
@@ -2,16 +2,25 @@ package util
 
 import (
 	"math/rand"
+	"time"
 
 	"gonum.org/v1/gonum/matrix/mat64"
 )
 
+var source *rand.Rand
+
 // NewRandMatrix builds a randomely initallized matrix of size m by n.
 func NewRandMatrix(m, n int) mat64.Matrix {
+	if source == nil {
+		rg := rand.NewSource(time.Now().UnixNano())
+
+		source = rand.New(rg)
+	}
+
 	backing := make([]float64, m*n)
 
 	for i := range backing {
-		backing[i] = rand.Float64()
+		backing[i] = source.Float64()
 	}
 
 	return mat64.NewDense(m, n, backing)
